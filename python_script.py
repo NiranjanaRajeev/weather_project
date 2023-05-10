e52457e1ca4403546e530cf922213e4e9c306892
Niranjana <niranjanasrajeev@gmail.com>
2023-05-10 15:45:27 +0200

    modified script to get parameters in metric

--- python_script.py
+++ python_script.py
@@ -3,6 +3,6 @@ import requests
 api_url = 'http://api.openweathermap.org/data/2.5/weather'
 appid = '1ab917e7eee961089f0c045f3f7a026b'
 
-r = requests.get(url=api_url, params=dict(q='Toulouse', APPID=appid))
+r = requests.get(url=api_url, params={"q": "Toulouse", "appid": appid, "units": "metric"})
 
 print(r.content.strip())
