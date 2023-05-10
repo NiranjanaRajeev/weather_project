38b6294fca232db015e3431030a812d7c1af73ef
Niranjana <niranjanasrajeev@gmail.com>
2023-05-10 15:39:03 +0200

    delete old folders

--- Debug/maths.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int addition(int a, int b);
-int soustraction(int a, int b);
-int multiplication(int a , int b);
-float division(float a, float b,int* etat);
--- Projet_Final/python_script.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-api_url = 'http://api.openweathermap.org/data/2.5/weather'
-appid = '1ab917e7eee961089f0c045f3f7a026b'
-
-r = requests.get(url=api_url, params=dict(q='Toulouse', APPID=appid))
-
-print(r.content.strip())
