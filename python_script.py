8fba044b855cbd87d79427704c95d202f8c2ca35
Niranjana <niranjanasrajeev@gmail.com>
2023-05-11 16:39:11 +0200

    arranged the program into functions and reverted to timestamp usage

--- python_script.py
+++ python_script.py
@@ -4,48 +4,64 @@ import sqlite3
 import time
 import datetime
 
-# Read the JSON config file
-with open('config.json') as f:
-    config = json.load(f)
-    
-# Read the configuration parameters
-API_URL = config['Python_script']['api_url']
-API_KEY = config['Python_script']['api_key']
-DB_FILENAME = config['Python_script']['db_filename']
-TABLE_NAME = config['Python_script']['table_name']
-cities = config['Python_script']['cities']
+def read_config(config_file):
+    # Read the JSON config file
+    with open(config_file) as f:
+        config = json.load(f)
 
-conn = sqlite3.connect(DB_FILENAME)
-cursor = conn.cursor()
-cursor.execute(f"CREATE TABLE IF NOT EXISTS {TABLE_NAME} (city TEXT, time TEXT, temperature REAL, humidity INTEGER, PRIMARY KEY (city, time))")
+    # Read the configuration parameters
+    api_url = config['Python_script']['api_url']
+    api_key = config['Python_script']['api_key']
+    db_filename = config['Python_script']['db_filename']
+    table_name = config['Python_script']['table_name']
+    cities = config['Python_script']['cities']
 
-while True:
-    for city in cities:
-        response = requests.get(API_URL, params={"q": city, "appid": API_KEY, "units": "metric"})
-        if response.status_code == 200:
-            data = response.json()
-            temperature = data["main"]["temp"]
-            humidity = data["main"]["humidity"]
-            timestamp = int(time.time())
-            
-            # Check if an entry exists for this city in the database
-            cursor.execute(f"SELECT * FROM {TABLE_NAME} WHERE city = ? ORDER BY time DESC LIMIT 1", (city,))
-            existing_data = cursor.fetchone()
+    return api_url, api_key, db_filename, table_name, cities
 
-            # If an entry exists, update it with the latest weather data
-            if existing_data:
-                existing_timestamp = existing_data[1]
-                if timestamp > existing_timestamp:
-                    dt_string = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S %d-%m-%Y ')
-                    cursor.execute(f"UPDATE {TABLE_NAME} SET time = ?, temperature = ?, humidity = ? WHERE city = ? AND timestamp = ?", (dt_string, temperature, humidity, city, existing_timestamp))
-                    conn.commit()
-            # Otherwise, insert a new entry for the city with the latest weather data
-            else:
-                dt_string = datetime.datetime.fromtimestamp(timestamp).strftime('%H:%M:%S %d-%m-%Y ')
-                cursor.execute(f"INSERT INTO {TABLE_NAME} (city, time, temperature, humidity) VALUES (?, ?, ?, ?)", (city, dt_string, temperature, humidity))
-                conn.commit()
-        else:
-            print(f"Error fetching weather data for {city}: {response.status_code} - {response.reason}")
-    time.sleep(60)
+def create_table(db_filename, table_name):
+    conn = sqlite3.connect(db_filename)
+    cursor = conn.cursor()
+    cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (city TEXT, timestamp INTEGER, temperature REAL, humidity INTEGER, PRIMARY KEY (city, timestamp))")
+    conn.close()
 
-conn.close()
+def fetch_weather_data(api_url, api_key, city):
+    response = requests.get(api_url, params={"q": city, "appid": api_key, "units": "metric"})
+    if response.status_code == 200:
+        data = response.json()
+        temperature = data["main"]["temp"]
+        humidity = data["main"]["humidity"]
+        timestamp = int(time.time())
+        return temperature, humidity, timestamp
+    else:
+        print(f"Error fetching weather data for {city}: {response.status_code} - {response.reason}")
+        return None, None, None
+
+def update_database(db_filename, table_name, city, temperature, humidity, timestamp):
+    conn = sqlite3.connect(db_filename)
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM {table_name} WHERE city = ? ORDER BY timestamp DESC LIMIT 1", (city,))
+    existing_data = cursor.fetchone()
+    if existing_data:
+        existing_timestamp = existing_data[1]
+        if timestamp > existing_timestamp:
+            cursor.execute(f"UPDATE {table_name} SET timestamp = ?, temperature = ?, humidity = ? WHERE city = ? AND timestamp = ?", (timestamp, temperature, humidity, city, existing_data[1]))
+            conn.commit()
+    else:
+        cursor.execute(f"INSERT INTO {table_name} (city, timestamp, temperature, humidity) VALUES (?, ?, ?, ?)", (city, timestamp, temperature, humidity))
+        conn.commit()
+    conn.close()
+
+def main():
+    api_url, api_key, db_filename, table_name, cities = read_config('config.json')
+    create_table(db_filename, table_name)
+
+    while True:
+        for city in cities:
+            temperature, humidity, timestamp = fetch_weather_data(api_url, api_key, city)
+            if temperature is not None and humidity is not None and timestamp is not None:
+                update_database(db_filename, table_name, city, temperature, humidity, timestamp)
+        time.sleep(60)
+
+if __name__ == '__main__':
+    main()
+
